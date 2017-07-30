a5a7c263ef2cfde538d582385f2bfa042d25a254
RadAd <adamgates84+github@gmail.com>
2017-07-30 17:01:01 +1000

    Compile c files aswell

--- build.bat
+++ build.bat
@@ -2,3 +2,4 @@
 cd /d %~dp0
 if not defined VCINSTALLDIR (echo Setup & call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" %COMPILE_TARGET%)
 forfiles /M *.cpp /C "cmd /C compile @file"
+forfiles /M *.c /C "cmd /C compile @file"
