4db1b88ed2b5569593a831cc25bf378b95841100
Adam Gates <adamgates84@gmail.com>
2017-08-09 13:25:28 +1000

    Output in unicode

--- RadClip.cpp
+++ RadClip.cpp
@@ -8,6 +8,8 @@
 
 int wmain(int argc, wchar_t** argv)
 {
+    int mode = _O_U8TEXT;
+    
     bool error = false;
     bool showUsage = false;
 
@@ -15,6 +17,8 @@ int wmain(int argc, wchar_t** argv)
     {
         if (wcscmp(argv[i], L"/?") == 0)
             showUsage = true;
+        else if (wcscmp(argv[i], L"/U") == 0)
+            mode = _O_U16TEXT;
         else
         {
             fwprintf(stderr, L"Unknown argument: %s\n", argv[i]);
@@ -32,6 +36,8 @@ int wmain(int argc, wchar_t** argv)
     
     if (error)
         return -2;
+        
+    _setmode(_fileno(stdout), mode);
 
     if (!OpenClipboard(NULL))
     {
@@ -39,84 +45,90 @@ int wmain(int argc, wchar_t** argv)
         return -1;
     }
 
-    HANDLE hClipboardData = NULL;
-    int ret = 1;
+    UINT f[] = { CF_UNICODETEXT, CF_HDROP };
+    int format = GetPriorityClipboardFormat(f, ARRAYSIZE(f));
+    if (format < 0)
+    {
+        fwprintf(stderr, L"Error Unknown Format\n");
+        CloseClipboard();
+        return -3;
+    }
     
-    // Output text if it exists
-    hClipboardData = GetClipboardData(CF_UNICODETEXT);
+    int ret = 0;
+    HANDLE hClipboardData = GetClipboardData(format);
     if (hClipboardData != NULL)
     {
-        ret = 0;
-        wchar_t *pchData = (wchar_t*)GlobalLock(hClipboardData);
+        switch (format)
+        {
+        case CF_UNICODETEXT:
+            {
+                wchar_t *pchData = (wchar_t*)GlobalLock(hClipboardData);
 
 #if 1
-        // Printing "\r\n" causes output of "\r\r\n"
-        wchar_t *pchNext;
-        while ((pchNext = wcschr(pchData, L'\r')) != nullptr)
-        {
-            size_t len = pchNext - pchData;
-            wprintf(L"%.*s", (int) len, pchData);
-            if (pchNext[1] != L'\n')
-                wprintf(L"\r");
-            pchData = pchNext + 1;
-        }
-        wprintf(pchData);
-        size_t len = wcslen(pchData);
-        if (len > 0 && pchData[len - 1] != L'\n')
-            wprintf(L"\n");
+                // Printing "\r\n" causes output of "\r\r\n"
+                wchar_t *pchNext;
+                while ((pchNext = wcschr(pchData, L'\r')) != nullptr)
+                {
+                    size_t len = pchNext - pchData;
+                    wprintf(L"%.*s", (int) len, pchData);
+                    if (pchNext[1] != L'\n')
+                        wprintf(L"\r");
+                    pchData = pchNext + 1;
+                }
+                wprintf(pchData);
+                size_t len = wcslen(pchData);
+                if (len > 0 && pchData[len - 1] != L'\n')
+                    wprintf(L"\n");
 #else
-        wchar_t bom = 0xFFFE;
-        fwrite(&bom, sizeof(wchar_t), 1, stdout);
-        fwrite(pchData, sizeof(wchar_t), wcslen(pchData), stdout);
+                wchar_t bom = 0xFFFE;
+                fwrite(&bom, sizeof(wchar_t), 1, stdout);
+                fwrite(pchData, sizeof(wchar_t), wcslen(pchData), stdout);
 #endif
 
-        GlobalUnlock(hClipboardData);
+                GlobalUnlock(hClipboardData);
+            }
+            break;
+
+        case CF_HDROP:
+            {
+                DROPFILES* pData = (DROPFILES*)GlobalLock(hClipboardData);
+
+                if (pData->fWide)
+                {
+                    wchar_t *pchData = (wchar_t*)((char*)pData + pData->pFiles);
+                    while (*pchData != L'\0')
+                    {
+                        wprintf(pchData);
+                        wprintf(L"\n");
+                        pchData += wcslen(pchData) + 1;
+                    }
+                }
+                else
+                {
+                    char *pchData = (char*)((char*)pData + pData->pFiles);
+                    while (*pchData != L'\0')
+                    {
+                        printf(pchData);
+                        printf("\n");
+                        pchData += strlen(pchData) + 1;
+                    }
+                }
+
+                GlobalUnlock(hClipboardData);
+            }
+            break;
+        }
+
     }
     else
     {
         DWORD Error = GetLastError();
         if (Error != 0)
             fwprintf(stderr, L"Error: %d\n", GetLastError());
-    }
-
-    // Output filenames if they exist
-    hClipboardData = GetClipboardData(CF_HDROP);
-    if (hClipboardData != NULL)
-    {
-        ret = 0;
-        DROPFILES* pData = (DROPFILES*)GlobalLock(hClipboardData);
-
-        if (pData->fWide)
-        {
-            wchar_t *pchData = (wchar_t*)((char*)pData + pData->pFiles);
-            while (*pchData != L'\0')
-            {
-                wprintf(pchData);
-                wprintf(L"\n");
-                pchData += wcslen(pchData) + 1;
-            }
-        }
-        else
-        {
-            char *pchData = (char*)((char*)pData + pData->pFiles);
-            while (*pchData != L'\0')
-            {
-                printf(pchData);
-                printf("\n");
-                pchData += strlen(pchData) + 1;
-            }
-        }
-
-        GlobalUnlock(hClipboardData);
-    }
-    else
-    {
-        DWORD Error = GetLastError();
-        if (Error != 0)
-            fwprintf(stderr, L"Error: %d\n", GetLastError());
+        ret = error;
     }
 
     CloseClipboard();
-
+    
     return ret;
 }
