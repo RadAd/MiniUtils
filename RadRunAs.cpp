9ee1831fe83febdd0b9b1caeff4585a6ba8f1cc1
RadAd <adamgates84+github@gmail.com>
2019-02-01 17:16:24 +1100

    Get credentials from windows credential manager

--- RadRunAs.cpp
+++ RadRunAs.cpp
@@ -2,7 +2,17 @@
 #include <Shlwapi.h>
 #include <WinCred.H>
 
+// To set credential:
+// cmdkey /generic:RadRunAs:<Cred> /user:<User> /pass
+
+// Note: Executable needs to be placed in a location accessible by the RunAs user
+
 #define PROGRAM L"RadRunAs"
+
+const LPCWSTR PARAM_ELEVATED = L"/elevated";
+const LPCWSTR PARAM_USER = L"/user:";
+const LPCWSTR PARAM_PASSWORD = L"/password:";
+const LPCWSTR PARAM_CRED = L"/cred:";
 
 void ErrorMessage(_In_ LPCWSTR lpFormat, ...)
 {
@@ -30,7 +40,20 @@ void ShowError(_In_ LPCWSTR lpWhere)
     }
 }
 
-LPCWSTR FindArg(LPCWSTR* szArgs, int nArgs, _In_ LPCWSTR lpParam)
+bool IsValidArg(LPCWSTR lpArg, _In_ LPCWSTR lpParam)
+{
+    int nLen = lstrlen(lpParam);
+    if (nLen >= 1 && lpParam[nLen - 1] == L':')
+    {
+        return StrCmpNI(lpArg, lpParam, nLen) == 0;
+    }
+    else
+    {
+        return StrCmpI(lpArg, lpParam) == 0;
+    }
+}
+
+LPCWSTR FindArg(const LPCWSTR* szArgs, int nArgs, _In_ LPCWSTR lpParam)
 {
     int nLen = lstrlen(lpParam);
     if (nLen >= 1 && lpParam[nLen - 1] == L':')
@@ -85,6 +108,58 @@ BOOL MyPathFindOnPath(_Inout_updates_(MAX_PATH) LPWSTR pszPath, _In_opt_ PZPCWST
     }
 }
 
+LPWSTR CopyString(const LPCWSTR s, const size_t l)
+{
+    LPWSTR r = new WCHAR[l + 1];
+    //wcscpy_s(r, l, s);
+    StrCpyN(r, s, (int) l + 1);
+    r[l] = L'\0';
+    return r;
+}
+
+LPWSTR CopyString(const LPCWSTR s)
+{
+    return CopyString(s, wcslen(s));
+}
+
+struct Credentials
+{
+    const LPCWSTR fulluser;
+    const LPCWSTR password;
+};
+
+const Credentials GetCredentials(const LPCWSTR* szArgs, int nArgs)
+{
+    const LPCWSTR cred = FindArg(szArgs, nArgs, PARAM_CRED);
+
+    if (cred != nullptr)
+    {
+        PCREDENTIAL pCred = nullptr;
+        if (CredRead(cred, CRED_TYPE_GENERIC, 0, &pCred))
+        {
+            // Note: Both of these CopyString leak memory, but due to short life of process, shouldn't be a problem
+            Credentials credentials{
+                CopyString(pCred->UserName),
+                CopyString((LPTSTR)pCred->CredentialBlob, pCred->CredentialBlobSize / sizeof(TCHAR))
+            };
+            CredFree(pCred);
+            return credentials;
+        }
+        else
+            return Credentials{
+                nullptr,
+                nullptr
+            };
+    }
+    else
+    {
+        return Credentials{
+            FindArg(szArgs, nArgs, PARAM_USER),
+            FindArg(szArgs, nArgs, PARAM_PASSWORD)
+        };
+    }
+}
+
 int CALLBACK wWinMain(
     _In_ HINSTANCE hInstance,
     _In_ HINSTANCE /*hPrevInstance*/,
@@ -100,23 +175,33 @@ int CALLBACK wWinMain(
         return 5;
     }
 
-    const LPCWSTR fulluser = FindArg(szArgs, nArgs, L"/user:");
-    bool elevated = FindArg(szArgs, nArgs, L"/elevated") != nullptr;
+    for (int i = 0; i < nArgs; ++i)
+    {
+        LPCWSTR lpArg = szArgs[i];
+        if (lpArg[0] == L'/'
+            && !(IsValidArg(lpArg, PARAM_ELEVATED) || IsValidArg(lpArg, PARAM_USER) || IsValidArg(lpArg, PARAM_PASSWORD) || IsValidArg(lpArg, PARAM_CRED)))
+        {
+            ErrorMessage(L"Invalid Argument: %s", lpArg);
+            return 1;
+        }
+    }
+
+    const Credentials cred = GetCredentials(szArgs, nArgs);
+    bool elevated = FindArg(szArgs, nArgs, PARAM_ELEVATED) != nullptr;
     const int command = FindCommandLine(szArgs, nArgs);
 
-    if (fulluser != nullptr && command >= 0)
+    if (cred.fulluser != nullptr && command >= 0)
     {
         const LPWSTR origCommandLine = StrStr(lpCmdLine, szArgs[command]);
         WCHAR title[MAX_PATH] = L"";
         StrCpy(title, origCommandLine);
         StrCat(title, L" (");
-        StrCat(title, fulluser);
+        StrCat(title, cred.fulluser);
         StrCat(title, L")");
 
-        const LPCWSTR password = FindArg(szArgs, nArgs, L"/password:");
         WCHAR username[1024] = L"";
         WCHAR domain[1024] = L"";
-        CredUIParseUserName(fulluser, username, 1024, domain, 1024);
+        CredUIParseUserName(cred.fulluser, username, 1024, domain, 1024);
 
         if (!elevated)
         {
@@ -132,7 +217,7 @@ int CALLBACK wWinMain(
             if (!CreateProcessWithLogonW(
                 username,
                 domain,
-                password,
+                cred.password,
                 LOGON_WITH_PROFILE,
                 application,
                 commandLine,
@@ -157,8 +242,7 @@ int CALLBACK wWinMain(
             CloseHandle(pi.hThread);
             CloseHandle(pi.hProcess);
         }
-
-        if (elevated)
+        else
         {
             WCHAR application[MAX_PATH] = L"";
             WCHAR commandLine[MAX_PATH] = L"";
@@ -175,7 +259,7 @@ int CALLBACK wWinMain(
             if (!CreateProcessWithLogonW(
                 username,
                 domain,
-                password,
+                cred.password,
                 LOGON_WITH_PROFILE,
                 application,
                 commandLine,
@@ -194,7 +278,7 @@ int CALLBACK wWinMain(
             CloseHandle(pi.hProcess);
         }
     }
-    else if (elevated)
+    else if (elevated && command >= 0)
     {
         const LPCWSTR application = szArgs[command];
         const LPCWSTR parameters = (command + 1) < nArgs ? StrStr(lpCmdLine, szArgs[command + 1]) : nullptr;
@@ -210,10 +294,15 @@ int CALLBACK wWinMain(
         WCHAR buf[] =
             PROGRAM L" Usage:\n"
             L"\n"
-            PROGRAM L" /user:<UserName> [/password:<Password>] [/elevated] program parameters\n"
+            PROGRAM L" /user:<UserName> [/password:<Password>] [/elevated] <program> [parameters]\n"
             L"\n"
-            PROGRAM L" /elevated program parameters";
+            PROGRAM L" /cred:RadRunAs:<Credential> [/elevated] <program> [parameters]\n"
+            L"\n"
+            L"    Created using: cmdkey /generic:RadRunAs:<Cred> /user:<User> /pass\n"
+            L"\n"
+            PROGRAM L" /elevated <program> [parameters]";
         MessageBox(NULL, buf, PROGRAM, MB_OK | MB_ICONINFORMATION);
+        return 1;
     }
 
     LocalFree(szArgs);
