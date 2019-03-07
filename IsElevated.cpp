c1dd5dbf82840b7212669944fd6a3208b62d6cd8
RadAd <adamgates84+github@gmail.com>
2019-03-07 11:58:28 +1100

    Added IsElevated

---
+++ IsElevated.cpp
@@ -0,0 +1,22 @@
+#include <Windows.h>
+
+BOOL IsElevated() {
+    BOOL fRet = FALSE;
+    HANDLE hToken = NULL;
+    if (OpenProcessToken(GetCurrentProcess(), TOKEN_QUERY, &hToken)) {
+        TOKEN_ELEVATION Elevation;
+        DWORD cbSize = sizeof(TOKEN_ELEVATION);
+        if (GetTokenInformation(hToken, TokenElevation, &Elevation, sizeof(Elevation), &cbSize)) {
+            fRet = Elevation.TokenIsElevated;
+        }
+    }
+    if (hToken) {
+        CloseHandle(hToken);
+    }
+    return fRet;
+}
+
+int main()
+{
+    return IsElevated() ? EXIT_SUCCESS : EXIT_FAILURE;
+}
