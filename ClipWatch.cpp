7763cf356b8dc22bcc3fcb2efc0c028a0087d72f
RadAd <adamgates84+github@gmail.com>
2019-03-21 12:59:53 +1100

    Fix ClipWatch using Priority formats

--- ClipWatch.cpp
+++ ClipWatch.cpp
@@ -1,7 +1,88 @@
 #include <stdio.h>
 #include <windows.h>
+#include <Shlobj.h>
 
-#pragma comment(lib, "user32.lib")
+bool DisplayClipboard(const UINT format)
+{
+    bool found = false;
+    HGLOBAL hClip = GetClipboardData(format);
+    if (hClip != NULL)
+    {
+        found = true;
+        switch (format)
+        {
+        default:
+            found = false;
+            break;
+
+        case CF_OEMTEXT:
+        case CF_TEXT:
+            {
+                PCSTR const buffer = (PCSTR) GlobalLock(hClip);
+                puts(buffer);
+                GlobalUnlock(hClip);
+
+            }
+            break;
+
+        case CF_UNICODETEXT:
+            {
+                PCWSTR const buffer = (PCWSTR) GlobalLock(hClip);
+                _putws(buffer);
+                GlobalUnlock(hClip);
+            }
+            break;
+
+        case CF_BITMAP:
+            {
+                HBITMAP hBitmap = (HBITMAP) hClip;
+                BITMAP bmp;
+                GetObject(hBitmap, sizeof(bmp), &bmp);
+                wprintf(L"Bitmap: %dx%d bpp:%d\n", bmp.bmWidth, bmp.bmHeight, bmp.bmBitsPixel);
+                GlobalUnlock(hClip);
+            }
+            break;
+
+        case CF_DIB:
+            {
+                BITMAPINFO* pBitmapInfo = (BITMAPINFO*) GlobalLock(hClip);
+                wprintf(L"DIB: %dx%d bitcount:%d\n", pBitmapInfo->bmiHeader.biWidth, pBitmapInfo->bmiHeader.biHeight, pBitmapInfo->bmiHeader.biBitCount);
+                GlobalUnlock(hClip);
+            }
+            break;
+
+        case CF_HDROP:
+            {
+                DROPFILES* pData = (DROPFILES*) GlobalLock(hClip);
+
+                if (pData->fWide)
+                {
+                    wchar_t *pchData = (wchar_t*) ((char*) pData + pData->pFiles);
+                    while (*pchData != L'\0')
+                    {
+                        wprintf(pchData);
+                        wprintf(L"\n");
+                        pchData += wcslen(pchData) + 1;
+                    }
+                }
+                else
+                {
+                    char *pchData = (char*) ((char*) pData + pData->pFiles);
+                    while (*pchData != L'\0')
+                    {
+                        printf(pchData);
+                        printf("\n");
+                        pchData += strlen(pchData) + 1;
+                    }
+                }
+
+                GlobalUnlock(hClip);
+            }
+            break;
+        }
+    }
+    return found;
+}
 
 LRESULT CALLBACK ClipWndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
@@ -18,19 +99,29 @@ LRESULT CALLBACK ClipWndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
     case WM_CLIPBOARDUPDATE:
         if (OpenClipboard(hWnd))
         {
-            HGLOBAL hClip = GetClipboardData(CF_UNICODETEXT);
-            if (hClip != NULL)
+            bool found = false;
+            int index = 0;
+            UINT format;
+            while (!found && (format = EnumClipboardFormats(index)) > 0)
             {
-                PCWSTR const buffer = (PCWSTR) GlobalLock(hClip);
-                wprintf(buffer);
-                size_t len = wcslen(buffer);
-                if (len > 0 && buffer[len - 1] != L'\n')
-                    wprintf(L"\n");
-                GlobalUnlock(hClip);
+                //wprintf(L"Format: %#.4x\n", format);
+                found = DisplayClipboard(format);
+                ++index;
             }
+            if (!found)
+            {
+                UINT f[] = { CF_UNICODETEXT, CF_BITMAP, CF_HDROP };
+                int format = GetPriorityClipboardFormat(f, ARRAYSIZE(f));
+                if (format > 0)
+                {
+                    //wprintf(L"Priority Format: %#.4x\n", format);
+                    DisplayClipboard(format);
+                }
+            }
+            _putws(L"---");
             CloseClipboard();
         }
-        return 0;
+        return !0;
 
     default:
         return DefWindowProc(hWnd, uMsg, wParam, lParam);
@@ -59,7 +150,8 @@ void main()
     {
         if (bRet == -1)
         {
-            // handle the error and possibly exit
+            fwprintf(stderr, L"Error: GetMessage %d\n", GetLastError());
+            break;
         }
         else
         {
