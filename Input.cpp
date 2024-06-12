f584d81eab017db9edecc7d6024596a78d0cbcff
RadAd <adamgates84+github@gmail.com>
2024-06-12 19:31:04 +1000

    Fixed up input

--- Input.cpp
+++ Input.cpp
@@ -23,6 +23,14 @@ void StrCopyIf(TCHAR (&dest)[size], const TCHAR *src)
         _tcscpy_s(dest, src);
 }
 
+BOOL WriteFileANSI(HANDLE hFile, _In_NLS_string_(cchWideChar)LPCWCH lpWideCharStr, int cchWideChar = -1)
+{
+    CHAR szAnsiCharStr[1024];
+    int bytes = WideCharToMultiByte(CP_ACP, 0, lpWideCharStr, cchWideChar, szAnsiCharStr, ARRAYSIZE(szAnsiCharStr), nullptr, nullptr);
+    DWORD dwWritten;
+    return WriteFile(hFile, szAnsiCharStr, bytes, &dwWritten, NULL);
+}
+
 BOOL PrintConsole(const HANDLE hOutput, const WCHAR* const format, ...)
 {
     WCHAR buffer[1024];
@@ -43,13 +51,23 @@ BOOL PrintConsole(const HANDLE hOutput, const CHAR* const format, ...)
     return WriteConsole(hOutput, buffer, DWORD(strlen(buffer)), nullptr, nullptr);
 }
 
+BOOL PrintANSI(const HANDLE hOutput, const TCHAR* const format, ...)
+{
+    TCHAR buffer[1024];
+    va_list args;
+    va_start(args, format);
+    _vsntprintf_s(ARRAY_X(buffer), format, args);
+    va_end(args);
+    return WriteFileANSI(hOutput, buffer, -1);
+}
+
 #define CHECK(t, msg) \
     if (!(t)) \
     { \
         DWORD e = GetLastError(); \
-        PrintConsole(GetStdHandle(STD_ERROR_HANDLE), _T("ERROR:\t%s (error %d)\n"), (msg), e); \
+        PrintANSI(GetStdHandle(STD_ERROR_HANDLE), _T("ERROR:\t%s (error %d)\n"), (msg), e); \
         return e; \
-    }
+    };
 
 #define COLOR(n, s) "\x1B[" #n "m" s "\x1B[0m"
 #define COMMAND(s) COLOR(37, s)
@@ -73,10 +91,20 @@ int _tmain(int argc, const TCHAR* const argv[])
 
     if (help)
     {
-        PrintConsole(hOutput, _T(COMMAND("%s")
-            " [/" OPTION("Default") "=" VALUE("initial_value") "]"
-            " [" VALUE("prompt") "]"
-            "\n"), argapp());
+        if (GetFileType(hOutput) == FILE_TYPE_CHAR)
+        {
+            CHECK(PrintConsole(hOutput, _T(COMMAND("%s")
+                " [/" OPTION("Default") "=" VALUE("initial_value") "]"
+                " [" VALUE("prompt") "]"
+                "\n"), argapp()), _T("PrintConsole Help\n"));
+        }
+        else
+        {
+            CHECK(PrintANSI(hOutput, _T("%s"
+                " [/" "Default" "=" "initial_value" "]"
+                " [" "prompt" "]"
+                "\n"), argapp()), _T("PrintANSI Help\n"));
+        }
         return EXIT_SUCCESS;
     }
 
@@ -96,6 +124,7 @@ int _tmain(int argc, const TCHAR* const argv[])
     CHECK(ReadConsole(hConsoleInput.get(), ARRAY_X(Buffer), &Read, &control), _T("ReadConsole Buffer"));
 
     Buffer[Read] = _T('\0');
-    CHECK(WriteConsole(hOutput, Buffer, Read, &Written, nullptr), _T("WriteConsole Buffer"));
+    //CHECK(WriteConsole(hOutput, Buffer, Read, &Written, nullptr), _T("WriteConsole Buffer"));
+    CHECK(WriteFileANSI(hOutput, Buffer, Read), _T("WriteFileANSI Buffer"));
     return EXIT_SUCCESS;
 }
