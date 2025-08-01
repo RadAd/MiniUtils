a71cc66dc7300ddc879ddf5f7d972cac32b79b87
RadAd <adamgates84+github@gmail.com>
2025-08-01 10:55:15 +1000

    Fix for ascii

--- RadMsgBox.cpp
+++ RadMsgBox.cpp
@@ -3,11 +3,19 @@
 #include <string>
 #include "arg.inl"
 
-#define PROGRAM L"RadMsgBox"
+#ifdef UNICODE
+#define tstring wstring
+#define __targv __wargv
+#else
+#define tstring string
+#define __targv __argv
+#endif
 
-void ErrorMessage(_In_ LPCWSTR lpFormat, ...)
+#define PROGRAM TEXT("RadMsgBox")
+
+void ErrorMessage(_In_ LPCTSTR lpFormat, ...)
 {
-    WCHAR buf[1024];
+    TCHAR buf[1024];
     va_list vl;
     va_start(vl, lpFormat);
     StringCchVPrintf(buf, ARRAYSIZE(buf), lpFormat, vl);
@@ -15,43 +23,43 @@ void ErrorMessage(_In_ LPCWSTR lpFormat, ...)
     MessageBox(NULL, buf, PROGRAM, MB_OK | MB_ICONERROR);
 }
 
-void ShowError(_In_ LPCWSTR lpWhere)
+void ShowError(_In_ LPCTSTR lpWhere)
 {
     DWORD errorCode = GetLastError();
-    LPWSTR msg = NULL;
+    LPTSTR msg = NULL;
     if (FormatMessage(FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_IGNORE_INSERTS,
-        NULL, errorCode, 0, (LPWSTR) &msg, 0, NULL) == 0)
+        NULL, errorCode, 0, (LPTSTR) &msg, 0, NULL) == 0)
     {
-        ErrorMessage(L"Error: %i FormatMessage.", errorCode);
+        ErrorMessage(TEXT("Error: %i FormatMessage."), errorCode);
     }
     else
     {
-        ErrorMessage(L"Error: %i %s.\n%s", errorCode, lpWhere, msg);
+        ErrorMessage(TEXT("Error: %i %s.\n%s"), errorCode, lpWhere, msg);
         LocalFree(msg);
     }
 }
 
-int CALLBACK wWinMain(
+int CALLBACK _tWinMain(
     _In_ HINSTANCE /*hInstance*/,
     _In_ HINSTANCE /*hPrevInstance*/,
-    _In_ LPWSTR    /*lpCmdLine*/,
+    _In_ LPTSTR    /*lpCmdLine*/,
     _In_ int       /*nCmdShow*/
 )
 {
-    arginit(__argc, __wargv, TEXT("Calls MessageBox"));
-    std::wstring title = argvalue(_T("/Title"), PROGRAM, _T("title"), _T("Message box title"));
-    std::wstring message = argnum(1, TEXT(""), _T("message"), _T("Message box message"));
+    arginit(__argc, __targv, TEXT("Calls MessageBox"));
+    std::tstring title = argvalue(_T("/Title"), PROGRAM, _T("title"), _T("Message box title"));
+    std::tstring message = argnum(1, TEXT(""), _T("message"), _T("Message box message"));
     UINT icon = 0;
-    if (argswitch(L"/IconError",        L"Error icon"))         icon = MB_ICONERROR;
-    if (argswitch(L"/IconInformation",  L"Information icon"))   icon = MB_ICONINFORMATION;
-    if (argswitch(L"/IconWarning",      L"Warning icon"))       icon = MB_ICONWARNING;
-    if (argswitch(L"/IconQuestion",     L"Question icon"))      icon = MB_ICONQUESTION;
+    if (argswitch(TEXT("/IconError"),       TEXT("Error icon")))        icon = MB_ICONERROR;
+    if (argswitch(TEXT("/IconInformation"), TEXT("Information icon")))  icon = MB_ICONINFORMATION;
+    if (argswitch(TEXT("/IconWarning"),     TEXT("Warning icon")))      icon = MB_ICONWARNING;
+    if (argswitch(TEXT("/IconQuestion"),    TEXT("Question icon")))     icon = MB_ICONQUESTION;
     UINT buttons = MB_OK;
-    if (argswitch(L"/Ok",               L"Ok button"))                  buttons = MB_OK;
-    if (argswitch(L"/OkCancel",         L"Ok and Cancel buttons"))      buttons = MB_OKCANCEL;
-    if (argswitch(L"/YesNo",            L"Yes and No buttons"))         buttons = MB_YESNO;
-    if (argswitch(L"/YesNoCancel",      L"Yes, No and Cancel buttons")) buttons = MB_YESNOCANCEL;
-    if (argswitch(L"/RetryCancel",      L"Retry and Cancel buttons"))   buttons = MB_RETRYCANCEL;
+    if (argswitch(TEXT("/Ok"),              TEXT("Ok button")))                     buttons = MB_OK;
+    if (argswitch(TEXT("/OkCancel"),        TEXT("Ok and Cancel buttons")))         buttons = MB_OKCANCEL;
+    if (argswitch(TEXT("/YesNo"),           TEXT("Yes and No buttons")))            buttons = MB_YESNO;
+    if (argswitch(TEXT("/YesNoCancel"),     TEXT("Yes, No and Cancel buttons")))    buttons = MB_YESNOCANCEL;
+    if (argswitch(TEXT("/RetryCancel"),     TEXT("Retry and Cancel buttons")))      buttons = MB_RETRYCANCEL;
     if (!argcleanup())
         return EXIT_FAILURE;
     if (argusage())
